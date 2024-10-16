6560d599cc793ae569db1fda5578861ebf6be26b
Abu Nasir Limon <leemon174@gmail.com>
2024-10-16 12:50:38 +0600

    worked on json parsing

--- app/api/movies/route.js
+++
@@ -1,7 +0,0 @@
-import { getAllMovies } from "@/lib/data";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const response = await getAllMovies();
-  return NextResponse.json(response);
-}
